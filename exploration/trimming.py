f96c11ba9aa56d1113a244acf5d4b3acd2f3ead3
Alex Rupp-Coppi <amr2311@columbia.edu>
2023-04-25 20:11:41 -0400

    making a 3k feature data batch

--- exploration/trimming.py
+++ exploration/trimming.py
@@ -11,7 +11,7 @@ model_type = model_processed
 deceased_cutoff  = 50. # 17. # smaller sample--less variation close to the median
 survivors_cutoff = 50. # 50. 
 
-expression_count_floor = 100
+expression_count_floor = 300
 
 survivors_processor = PostprocessorTrim(model_type, DATA_SURVIVORS_PATH, DESTINATION_SURVIVORS_PATH, survivors_cutoff, expression_count_floor)
 deceased_processor = PostprocessorTrim(model_type, DATA_DECEASED_PATH, DESTINATION_DECEASED_PATH, deceased_cutoff, expression_count_floor)
